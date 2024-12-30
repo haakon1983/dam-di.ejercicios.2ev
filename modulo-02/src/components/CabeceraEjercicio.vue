757bb62b41a5f017c907c28be7bb11a6a5358116
haakon1983 <vfontechac@gmail.com>
2024-12-30 10:32:12 +0100

    Creado componente Cabecera y ejercicio1

---
+++ modulo-02/src/components/CabeceraEjercicio.vue
@@ -0,0 +1,44 @@
+<template>
+    <header class="cabecera-ejercicio">
+      <h1 class="titulo-ejercicio">
+        <slot>
+          Título del Ejercicio
+        </slot>
+      </h1>
+      <p class="descripcion-ejercicio">
+        <slot name="descripcion">
+          Descripción del ejercicio.
+        </slot>
+      </p>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+    name: 'CabeceraEjercicio',
+  }
+  </script>
+  
+  <style scoped>
+  .cabecera-ejercicio {
+    background-color: #f5f5f5;
+    padding: 20px;
+    border-bottom: 2px solid #ddd;
+    text-align: center;
+    border-radius: 10px;
+    
+  }
+  
+  .titulo-ejercicio {
+    font-size: 2em;
+    margin: 0;
+    color: #333;
+  }
+  
+  .descripcion-ejercicio {
+    font-size: 1.2em;
+    color: #666;
+    margin-top: 10px;
+  }
+  </style>
+  
